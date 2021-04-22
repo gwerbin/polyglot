b3e68c92cb57dc99fab3b1735c06d917d99dfba7
Greg Werbin <gwerbin@understood.org>
2021-04-21 21:36:31 -0400

    Try to use modules idk

--- http-hello/crystal/server.cr
+++ http-hello/crystal/server.cr
@@ -7,127 +7,128 @@ require "json"
 require "log"
 require "option_parser"
 
-
-def die(status, msg)
-  STDERR.puts msg
-  exit status
-end
-
-
-silly_data = {
-  "time" => Time.local(),
-  "datum" => [
-    "eAcute is \u{E9}, combinedEAcute is \u{65}\u{301}; precomposed is \u{D55C}, decomposed is \u{1112}\u{1161}\u{11AB}",
-    10000000,
-    nil,
-    [0, 0.0],
-  ]
-}
-
-
-def respond_json(context, data)
-  payload = data.to_json.to_slice
-  context.response.content_length = payload.size
-  context.response.content_type = "application/json; charset=utf-8"
-  context.response.write payload
-end
-
-
-class Handler
-  include HTTP::Handler
-
-  def initialize(@delay : Float64)
+module HttpHello
+  def respond_json(context, data)
+    payload = data.to_json.to_slice
+    context.response.content_length = payload.size
+    context.response.content_type = "application/json; charset=utf-8"
+    context.response.write payload
   end
 
-  def call(context)
-    sleep @delay
-    respond_json(context, silly_data)
-  end
-end
+  class Handler
+    include HTTP::Handler
 
+    @silly_data = {
+      "time" => Time.local(),
+      "datum" => [
+        "eAcute is \u{E9}, combinedEAcute is \u{65}\u{301}; precomposed is \u{D55C}, decomposed is \u{1112}\u{1161}\u{11AB}",
+        10000000,
+        nil,
+        [0, 0.0],
+      ]
+    }
 
-class ServerOptions
-  property delay
-  def initialize(@delay = 0.25)
-  end
-end
-
-
-def make_server(options)
-  delay = options.delay
-
-  if (delay < 0.0)
-    raise ArgumentError.new("Delay cannot be negative")
-  end
-
-  if (delay > 5.0)
-    raise ArgumentError.new("Delay is too long: #{delay}")
-  end
-
-  return HTTP::Server.new Handler.new(options.delay)
-end
-
-
-def parse_cli
-  if ENV.has_key? "HELLO_DELAY"
-    delay = ENV["HELLO_DELAY"]
-  else
-    delay = ""
-  end
-
-  OptionParser.parse do |parser|
-    parser.banner = "Polyglot - HTTP Hello"
-
-    parser.on "-h", "--help", "Show help and exit." do
-      puts parser
-      exit
+    def initialize(@delay : Float64)
     end
 
-    parser.on "-v", "--version", "Show version and exit." do
-      puts "v1.0"
-      exit
-    end
-
-    parser.missing_option do |option_flag|
-      die 1, "ERROR: #{option_flag} requires an argument."
-    end
-
-    parser.invalid_option do |option_flag|
-      die 1, "ERROR: #{option_flag} is not a valid option."
-    end
-
-    parser.on "--delay=VALUE", "Request-handling delay." do |value|
-      delay = value
+    def call(context)
+      sleep @delay
+      respond_json(context, @silly_data)
     end
   end
 
-  options = ServerOptions.new
-  if delay.size() > 0
-	  begin
-      options.delay = delay.to_f
-	  rescue e : ArgumentError
-      die 1, "Invalid value for --delay: #{delay}."
-	  end
+  class ServerOptions
+    property delay
+    def initialize(@delay = 0.25)
+    end
   end
 
-  return options
+  def make_server(options)
+    delay = options.delay
+
+    if (delay < 0.0)
+      raise ArgumentError.new("Delay cannot be negative")
+    end
+
+    if (delay > 5.0)
+      raise ArgumentError.new("Delay is too long: #{delay}")
+    end
+
+    return HTTP::Server.new Handler.new(options.delay)
+  end
+
+  class CLI
+    def die(status, msg)
+      STDERR.puts msg
+      exit status
+    end
+
+    def parse_cli
+      if ENV.has_key? "HELLO_DELAY"
+        delay = ENV["HELLO_DELAY"]
+      else
+        delay = ""
+      end
+
+      OptionParser.parse do |parser|
+        parser.banner = "Polyglot - HTTP Hello"
+
+        parser.on "-h", "--help", "Show help and exit." do
+          puts parser
+          exit
+        end
+
+        parser.on "-v", "--version", "Show version and exit." do
+          puts "v1.0"
+          exit
+        end
+
+        parser.missing_option do |option_flag|
+          die 1, "ERROR: #{option_flag} requires an argument."
+        end
+
+        parser.invalid_option do |option_flag|
+          die 1, "ERROR: #{option_flag} is not a valid option."
+        end
+
+        parser.on "--delay=VALUE", "Request-handling delay." do |value|
+          delay = value
+        end
+      end
+
+      options = ServerOptions.new
+      if delay.size() > 0
+        begin
+          options.delay = delay.to_f
+        rescue e : ArgumentError
+          die 1, "Invalid value for --delay: #{delay}."
+        end
+      end
+
+      return options
+    end
+
+    def main
+      options = parse_cli
+
+      begin
+        server = make_server(options)
+      rescue e : ArgumentError
+        die 1, e
+      end
+
+      begin
+        address = server.bind_tcp ARGV[0].to_i
+      rescue e : IndexError
+        die 1, "No port number provided."
+      end
+
+      puts "Listening for HTTP requests at #{address}, with request delay #{options.delay} seconds."
+      server.listen
+    end
+  end
 end
 
-
-options = parse_cli
-
-begin
-  server = make_server(options)
-rescue e : ArgumentError
-  die 1, e
-end
-
-begin
-	address = server.bind_tcp ARGV[0].to_i
-rescue e : IndexError
-	die 1, "No port number provided."
-end
-
-puts "Listening for HTTP requests at #{address}, with request delay #{options.delay} seconds."
-server.listen
-
+# TODO: I have no idea how to use modules. Scoping is so stupid and complicated in this language.
+include HttpHello
+CLI.new.main
